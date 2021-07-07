c0f8e6ae17bde8eaf12af5eac8a31e08633895db
Mikkel Højlund Larsen <mikkelhojlundgrontmij@gmail.com>
2021-07-07 14:16:28 +0200

    working on it

---
+++ examples/metrics_simple.cpp
@@ -0,0 +1,13 @@
+// Copyright (c) Steinwurf ApS 2020.
+// All Rights Reserved
+//
+// Distributed under the "BSD License". See the accompanying LICENSE.rst file.
+
+#include <iostream>
+
+#include <abacus/metrics.hpp>
+
+int main()
+{
+    std::cout << "Hello World";
+}
